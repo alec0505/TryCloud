afb06f6cd41d5af821d9f099ba1623d13aba9ace
Alec1 <alec0505@gmail.com>
2022-11-04 12:08:23 -0400

    hello students

---
+++ src/test/java/com/trycloud/utilities/ConfigurationReader.java
@@ -0,0 +1,47 @@
+package com.trycloud.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    // create the obj of Properties class
+    // we need properties class to use methods coming from load(),getProperty("key")
+    // it is static because we using it in static method
+    // it is private because I will use it only in this class
+
+    private static Properties properties = new Properties(); // using private due to only use inside this class
+
+    // we want to open the properties file and load the properties object only ones before reading
+    //we put inside static block so it will run first and only ones
+
+    static {
+        try {
+            // 2. Create the obj of FileInputStream;
+            // we need this obj to open file as a stream in Java memory
+            FileInputStream file = new FileInputStream("config.properties");
+
+            //3. load the properties obj
+            // load properties obj with the "file" we opened using FileInputStream
+            properties.load(file);
+
+            // close the file after loading
+            //if we do not close the file , it will take space from comp memory like Scanner
+            file.close();
+        } catch (IOException e) {
+
+        }
+    }
+
+    /**
+     * This method is used to read value from a configuration.properties file
+     * @param keyword --> key name in configuration.properties file
+     * @return value for key, returns null if key is not found
+     * driver.get(ConfigurationReader.getProperties("env"))
+     */
+    public static String getProperty(String keyword){
+        return properties.getProperty(keyword);
+    }
+
+}
---
+++ src/test/java/com/trycloud/utilities/Driver.java
@@ -0,0 +1,42 @@
+package com.trycloud.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Driver {
+    private Driver() {
+    } // private constructor, no access to the object of this class
+
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>(); // making our driver instance private , so it is not reachable, we make it static, becouse we want to run it before anything else
+
+    public static WebDriver getDriver() { // create re-usable utility which will return same driver instance when we call it. Depending on browserType our switch statement will determine to open specific type of browser
+
+        if (driverPool.get() == null) {
+            String browserType = ConfigurationReader.getProperty("browser");
+            switch (browserType) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driverPool.set(new ChromeDriver());
+                    driverPool.get().manage().window().maximize();
+                    //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver());
+                    driverPool.get().manage().window().maximize();
+                    //.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+            }
+        }
+        return driverPool.get(); // same driver instance will be return everytime we call Driver.getDriver() method
+    }
+    public static void closeDriver(){
+        if(driverPool.get()!=null){
+            driverPool.get().quit();
+            driverPool.remove();
+        }
+    }
+}
