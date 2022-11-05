059ad89eb2f59831cb4638f12541a7bf79852ab9
Isa-Yilmaz <abdullahisaylmaz@gmail.com>
2022-11-05 13:59:48 -0400

    Login functionality with valid credentials

--- src/test/java/com/trycloud/runner/CukesRunner.java
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -12,8 +12,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/trycloud/step_definitions" ,
         dryRun = false ,
-        tags = "@maleScientist" ,
-        publish = true
+        tags = ""
+
 )
 public class CukesRunner {
 
